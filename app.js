e9abdcdca97dabb8acb3cf36d2cc1c82b1ef8fa9
Lou Yunlong <lianer@live.cn>
2016-10-17 11:21:56 +0800

    增加首页；切到express

--- app.js
+++ app.js
@@ -1,95 +1,56 @@
-var fs = require('fs')
-var path = require('path')
-var restify = require('restify')
-var server = restify.createServer({
-	name: 'restify',
-	version: '1.0.0'
-})
-var convert = require('./convert.js')
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-if(!fs.existsSync(path.join(__dirname, './cache'))){
-	fs.mkdirSync(path.join(__dirname, './cache'))
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', require('./routes/index'));
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
-server.use(restify.queryParser())
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
+});
 
-server.use(function (req, res, next) {
-	res.header('X-Powerd-By', 'restify')
-	next()
-})
 
-server.get('/', function (req, res, next) {
-	// 尺寸必须是字符串
-	var width = '750'
-	var height = '320'
-	var bg = 'ccc'
-	var fg = '666'
-
-	var outputPath = path.join(__dirname, `./cache/${width}x${height}_${bg}_${fg}.png`)
-	fs.exists(outputPath, function (exists) {
-		if (exists) {
-			res.header('Content-Type', 'image/png')
-			fs.createReadStream(outputPath).pipe(res)
-			// res.send()
-			return
-		}
-		convert(width, height, bg, fg, function (err, stdout, stderr, command) {
-			if(err){
-				return next(err)
-			}
-
-			var cacheStream = fs.createWriteStream(outputPath)
-			stdout.pipe(cacheStream)
-
-			res.header('Content-Type', 'image/png')
-			// Pipe image data stream to the response
-			stdout.pipe(res)
-
-			// Pipe any stdout data to the process.stdout
-			// so that it can be retrieved in the logs
-			stderr.pipe(process.stdout)
-		})
-	})
-})
-
-server.get(/^\/(?:(\d{1,4})x(\d{1,4}))\/?/i, function (req, res, next) {
-	var width = req.params[0]
-	var height = req.params[1]
-	var bg = req.params.bg || 'ccc'
-	var fg = req.params.fg || '666'
-
-	var outputPath = path.join(__dirname, `./cache/${width}x${height}_${bg}_${fg}.png`)
-
-	fs.exists(outputPath, function (exists) {
-		if (exists) {
-			res.header('Content-Type', 'image/png')
-			fs.createReadStream(outputPath).pipe(res)
-			return
-		}
-		convert(width, height, bg, fg, function (err, stdout, stderr, command) {
-			if(err){
-				return next(err)
-			}
-
-			var cacheStream = fs.createWriteStream(outputPath)
-			stdout.pipe(cacheStream)
-
-			res.header('Content-Type', 'image/png')
-			// Pipe image data stream to the response
-			stdout.pipe(res)
-
-			// Pipe any stdout data to the process.stdout
-			// so that it can be retrieved in the logs
-			stderr.pipe(process.stdout)
-		})
-	})
-})
-
-server.on('uncaughtException', function (req, res, route, err) {
-	console.log(err)
-	res.send(new restify.InternalError('未知错误'))
-})
-
-server.listen(process.env.PORT || '4006', function () {
-	console.log(`server listen at`, server.name, server.url)
-})
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,60 @@
+var fs = require('fs')
+var path = require('path')
+var util = require('util')
+var express = require('express')
+var router = express.Router()
+var convert = require('../convert.js')
+
+if(!fs.existsSync(path.join(__dirname, './cache'))){
+	fs.mkdirSync(path.join(__dirname, './cache'))
+}
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+	var host = 'http://' + req.headers['host']
+	fs.readFile(path.join(__dirname, '../views/index.html'), function (err, content) {
+		var transfer = content.toString().split("http://fed.guahao-inc.com/dummyimg").join(host)
+		res.header('Content-Type', 'text/html')
+		res.send(transfer)
+	})
+})
+
+router.get('/snippet', function (req, res, next) {
+	res.attachment('dummyimg.sublime-snippet')
+	fs.createReadStream(path.join(__dirname, '../data/dummyimg.sublime-snippet')).pipe(res)
+})
+
+router.get(/^\/(?:(\d{1,4})x(\d{1,4}))\/?/i, function (req, res, next) {
+	var width = req.params[0]
+	var height = req.params[1]
+	var bg = req.query.bg || 'ccc'
+	var fg = req.query.fg || '666'
+
+	var outputPath = path.join(__dirname, `./cache/${width}x${height}_${bg}_${fg}.png`)
+
+	fs.exists(outputPath, function (exists) {
+		if (exists) {
+			res.header('Content-Type', 'image/png')
+			fs.createReadStream(outputPath).pipe(res)
+			return
+		}
+		convert(width, height, bg, fg, function (err, stdout, stderr, command) {
+			if(err){
+				return next(err)
+			}
+
+			var cacheStream = fs.createWriteStream(outputPath)
+			stdout.pipe(cacheStream)
+
+			res.header('Content-Type', 'image/png')
+			// Pipe image data stream to the response
+			stdout.pipe(res)
+
+			// Pipe any stdout data to the process.stdout
+			// so that it can be retrieved in the logs
+			stderr.pipe(process.stdout)
+		})
+	})
+})
+
+module.exports = router
